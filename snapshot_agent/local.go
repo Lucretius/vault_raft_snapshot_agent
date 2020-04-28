aa6e437817e682af4ca2e3aed70836b18baf7066
Robert Lippens <robertl@sirrus7.com>
2020-04-28 12:11:01 -0700

    retain logic added for local

--- snapshot_agent/local.go
+++ snapshot_agent/local.go
@@ -4,16 +4,72 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"sort"
+	"strings"
 
 	"github.com/Lucretius/vault_raft_snapshot_agent/config"
 )
 
 // CreateLocalSnapshot writes snapshot to disk location
 func (s *Snapshotter) CreateLocalSnapshot(buf *bytes.Buffer, config *config.Configuration, currentTs int64) (string, error) {
-	err := ioutil.WriteFile(fmt.Sprintf("%s/raft_snapshot-%d.snap", config.Local.Path, currentTs), buf.Bytes(), 0644)
+	fileName := fmt.Sprintf("%s/raft_snapshot-%d.snap", config.Local.Path, currentTs)
+	err := ioutil.WriteFile(fileName, buf.Bytes(), 0644)
 	if err != nil {
 		return "", err
 	} else {
-		return fmt.Sprintf("%s/raft_snapshot-%d.snap", config.Local.Path, currentTs), nil
+		if config.Retain > 0 {
+			fileInfo, err := ioutil.ReadDir(config.Local.Path)
+			filesToDelete := make([]os.FileInfo, 0)
+			for _, file := range fileInfo {
+				if strings.Contains(file.Name(), "raft_snapshot-") {
+					filesToDelete = append(filesToDelete, file)
+				}
+			}
+			if err != nil {
+				log.Println("Unable to read file directory to delete old snapshots")
+				return fileName, err
+			}
+			timestamp := func(f1, f2 *os.FileInfo) bool {
+				file1 := *f1
+				file2 := *f2
+				return file1.ModTime().Before(file2.ModTime())
+			}
+			By(timestamp).Sort(filesToDelete)
+			filesToDelete = fileInfo[0 : len(filesToDelete)-(int(config.Retain)-1)]
+			for _, f := range filesToDelete {
+				os.Remove(fmt.Sprintf("%s/%s", config.Local.Path, f.Name()))
+			}
+		}
+		return fileName, nil
 	}
+}
+
+// implementation of Sort interface for fileInfo
+type By func(f1, f2 *os.FileInfo) bool
+
+func (by By) Sort(files []os.FileInfo) {
+	fs := &fileSorter{
+		files: files,
+		by:    by, // The Sort method's receiver is the function (closure) that defines the sort order.
+	}
+	sort.Sort(fs)
+}
+
+type fileSorter struct {
+	files []os.FileInfo
+	by    func(f1, f2 *os.FileInfo) bool // Closure used in the Less method.
+}
+
+func (s *fileSorter) Len() int {
+	return len(s.files)
+}
+
+func (s *fileSorter) Less(i, j int) bool {
+	return s.by(&s.files[i], &s.files[j])
+}
+
+func (s *fileSorter) Swap(i, j int) {
+	s.files[i], s.files[j] = s.files[j], s.files[i]
 }
