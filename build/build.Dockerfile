24de329057e592298c4e250a1f60878da75237b0
Karsten Kraus <Karsten.Kraus@swr.de>
2023-08-21 11:14:36 +0200

    workflow builds multiplatform-image

--- build/build.Dockerfile
+++ build/build.Dockerfile
@@ -2,9 +2,6 @@
 ARG go_version=1.16
 FROM golang:$go_version AS builder
 
-ENV GOOS=linux \
-    GOARCH=amd64
-
 COPY ./build/build.sh /bin/build.sh
 RUN chmod +x /bin/build.sh
 
--- build/build.sh
+++ build/build.sh
@@ -3,18 +3,18 @@ set -eu
 
 BUILD_DIR=${1:-./build}
 OUT_DIR=${2:-./out}
-ARCH=${3:-amd64}
+PLATFORM=${3:-linux/amd64}
 POST_ACTION=${4:-}
 
 export GO111MODULE=on
 export CGO_ENABLED=0
-export GOOS=linux
-export GOARCH=${ARCH}
+export GOOS=$(dirname "$PLATFORM")
+export GOARCH=$(basename "$PLATFORM")
 
 
-ARCH_DIR=${OUT_DIR}/${ARCH}
-mkdir -p ${ARCH_DIR}
-echo "Building go source in $(realpath "$BUILD_DIR") to $(realpath "$ARCH_DIR")..."
+PLATFORM_OUT_DIR=${OUT_DIR}/${PLATFORM}
+mkdir -p ${PLATFORM_OUT_DIR}
+echo "Building go source in $(realpath "$BUILD_DIR") for $GOOS/$GOARCH to $(realpath "$PLATFORM_OUT_DIR")..."
 cd ${BUILD_DIR}
 go get -v ./...;
 go build \
@@ -22,12 +22,12 @@ go build \
     -trimpath \
     -ldflags "-s -w -extldflags '-static'" \
     -tags 'osusergo netgo static_build' \
-    -o "${ARCH_DIR}" \
+    -o "${PLATFORM_OUT_DIR}" \
     ./...
 
 if [ "$POST_ACTION" == "run" ]; then
-    chmod +x $ARCH_DIR/vault-raft-snapshot-agent
-    exec $ARCH_DIR/vault-raft-snapshot-agent
+    chmod +x $PLATFORM_OUT_DIR/vault-raft-snapshot-agent
+    exec $PLATFORM_OUT_DIR/vault-raft-snapshot-agent
 elif [ -n "$POST_ACTION" ]; then
     exec sh -c "$POST_ACTION"
 fi
